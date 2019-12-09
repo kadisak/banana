411572a1fa6e29c3c644b0a952e2dd308b7c035d
kadisak <kpadisak@gmail.com>
2019-12-09 14:22:53 +0700

    update main funciton

--- helloworld.py
+++ helloworld.py
@@ -1,4 +1,9 @@
-print("Hello world!")
-print("CPS")
-#my comment
-#comment
+#my first Python program
+import my_function as my_fn
+
+if __name__ == '__main__':
+    x=5
+    print("Hello world!")
+    y=my_fn.f(x)
+    print("x,y =",x,y)
+    
